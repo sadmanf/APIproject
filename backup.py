9cdae9a3799f6d281cb185b2cb0c80dcb6289ab2
Sadman Fahmid <sadmanfahmid@gmail.com>
2014-11-12 01:21:22 -0500

    @

--- backup.py
+++ backup.py
@@ -1,5 +1,8 @@
+from flask import Flask, render_template, request
 import urllib2
 import json
+
+app = Flask(__name__)
 
 def acafilter(a):
         if a.find('.org')>-1:
@@ -21,16 +24,40 @@ def acafilter(a):
 #Returns all "academic" urls, query is a string.
 
 def getAcademicUrls(query):
-	url="""
+	query = query.replace(" ", "+")
+        print "The query is " + query
+        url="""
 	http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="""+query+"""&safe=active
 	"""
+
 	request= urllib2.urlopen(url)
 	result = request.read()
 	d = json.loads(result)
 	rlist = d['responseData']['results']
+        urls = []
+
+        print len(rlist)
+
 	for item in rlist:
         	if acafilter(item['url']):
-                	return item['url']
+#                        print(item['url'])
+                        urls.append(item['url'])
+        
+        for url in urls:
+                print url
+                
+        return urls;
+#print getAcademicUrls('abc')
 
-print getAcademicUrls('chemistry')
+@app.route("/", methods=["GET", "POST"])
+def home():
+        if request.method == 'POST':
+                results = []
+                results = getAcademicUrls(request.form['input'])
+                return render_template("home.html", results = results)
+        return render_template("home.html", results = '')
 
+
+if __name__ == "__main__":
+        app.debug = True
+        app.run()
