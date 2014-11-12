dac4442f8f8f2745434fe35dac4a5c2172683b94
corrrin9797 <corrinoff@gmail.com>
2014-11-11 22:12:06 -0800

    The backup needed some work. I changed it to a function so as it returns the urls that the algorithm considers acceptable.

--- backup.py
+++ backup.py
@@ -17,17 +17,19 @@ def acafilter(a):
 
         return False
 
-def
 
-url="""
-http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="""+PUTQUERYHERE+"""&safe=active
-"""
-request= urllib2.urlopen(url)
-result = request.read()
-d = json.loads(result)
-rlist = d['responseData']['results']
-for item in rlist:
-        if acafilter(item['url']):
-                print item['content']
+#Returns all "academic" urls, query is a string.
+
+def getAcademicUrls(query):
+	url="""
+	http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="""+query+"""&safe=active
+	"""
+	request= urllib2.urlopen(url)
+	result = request.read()
+	d = json.loads(result)
+	rlist = d['responseData']['results']
+	for item in rlist:
+        	if acafilter(item['url']):
+                	return item['url']
 
 
