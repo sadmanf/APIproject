64a8bf0dd2dd391af219feb84ab48f94a93ba712
corrrin9797 <corrinoff@gmail.com>
2014-11-11 21:02:54 -0800

    backup google search filter thingy

---
+++ backup.py
@@ -0,0 +1,32 @@
+import urllib2
+import json
+
+def acafilter(a):
+        if a.find('.org')>-1:
+                return True
+        if a.find('.edu')>-1:
+                return True
+        if a.find('.gov')>-1:
+                return True
+        if a.find('.com')>-1:
+                if a.find('nytimes.com')>-1:
+                        return True
+                if a.find('wsj.com')>-1:
+                        return True
+
+        return False
+
+def
+
+url="""
+http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=cows&safe=active
+"""
+request= urllib2.urlopen(url)
+result = request.read()
+d = json.loads(result)
+rlist = d['responseData']['results']
+for item in rlist:
+        if acafilter(item['url']):
+                print item['content']
+
+
